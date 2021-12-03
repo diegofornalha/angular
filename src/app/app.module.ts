7a1a0690c4e59f514365c41a7aafa5170c032644
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2021-12-03 16:10:09 -0300

    Criando meu primeiro e segundo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponente } from './meu-primeiro/meu-primeiro.component';
+import { MeuSegundoComponenteComponent } from './meu-segundo-componente/meu-segundo-componente.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [	
+    AppComponent,
+    MeuPrimeiroComponente,
+    MeuSegundoComponenteComponent
+   ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro-componente',
+  templateUrl: './meu-primeiro.component.html',
+})
+
+export class MeuPrimeiroComponente implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
