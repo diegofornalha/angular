00063b514791c612187a39612534ee4641c381fc
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2021-12-04 00:11:08 -0300

    remover pasta

--- hello-typescript/main.js
+++
@@ -1,5 +0,0 @@
-//arrow function 
-numeros.map(function (valor) { return valor * 2; });
-var n1 = 'abc';
-n1 = 4;
-//para compilar tsc main.ts --watch
--- hello-typescript/main.ts
+++
@@ -1,5 +0,0 @@
-//arrow function 
-numeros.map(valor => valor * 2);
-var n1: any = 'abc';
-n1 = 4;
-//para compilar tsc main.ts --watch
