de33d3344a15e7d9f8812c2911ec01654fcdfb81
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2021-12-06 19:41:43 -0300

    Update src\polyfills.ts
    libs auxiliares, o código vai ser compilado pelo ES6 e ES7.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,7 +55,41 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
+import 'core-js/es6/reflect';
+
+import 'core-js/es7/reflect';
+import 'core-js/es7/array';
+import 'core-js/es7/object';
+import 'core-js/es7/string';
+import 'core-js/es7/map';
+import 'core-js/es7/set';
+import 'core-js/es7/weak-map';
+import 'core-js/es7/symbol';
+import 'core-js/es7/object';
+import 'core-js/es7/function';
+import 'core-js/es7/parse-int';
+import 'core-js/es7/parse-float';
+import 'core-js/es7/number';
+import 'core-js/es7/math';
+import 'core-js/es7/date';
+import 'core-js/es7/regexp';
+
 import 'zone.js/dist/zone'; // Included with Angular CLI.
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
