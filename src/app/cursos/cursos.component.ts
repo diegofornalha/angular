64c415e6f63f402babf00c81eb6112b668e3988e
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2021-12-07 12:28:43 -0300

    trabalhando com templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './cursos.component.html',
 
 })
-export class CursosComponent  {}
+
+export class CursosComponent  {
+
+  nomePortal: string;
+  constructor() {
+  this.nomePortal = 'https://loiane.training';
+  }
+  cursos = [
+    {nome: 'Angular 2', url: 'https://loiane.training/curso/angular'},
+    {nome: 'Java Básico', url: 'https://loiane.training/curso/java-basico'},
+    {nome: 'Java Intermediário', url: 'https://loiane.training/curso/java-intermediario'},
+    {nome: 'Crud Angular Spring', url: 'http://loiane.training/curso/crud-angular-spring'},
+    {nome: 'Fundamentos ExtJS 4', url: 'https://loiane.training/curso/fundamentos-ext-js-4'},
+    {nome: 'Phonegap Apache Cordova', url: 'https://loiane.training/curso/phonegap-apache-cordova'},
+    {nome: 'Estrutura de Dados', url: 'https://loiane.training/curso/estrutura-de-dados'},
+  ];
+}
