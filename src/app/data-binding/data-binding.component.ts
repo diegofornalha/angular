0ec789c019cbd28066a40b3f94a34705b844fd2f
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2021-12-07 17:08:44 -0300

    data-binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
 })
-export class DataBindingComponent implements OnInit {
+export class DataBindingComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  urlImagem = 'https://pbs.twimg.com/profile_images/1446943963521355780/_76XXVTD_400x400.jpg';
 }
