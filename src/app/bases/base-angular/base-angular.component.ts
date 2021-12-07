b7e2775374f56dce388b56d22f9c4e55509a3800
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2021-12-07 11:35:58 -0300

    Novo modulo com componente dentro

---
+++ src/app/bases/base-angular/base-angular.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-angular',
+  templateUrl: './base-angular.component.html',
+
+})
+export class BaseAngularComponent {
+}
---
+++ src/app/bases/bases.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bases',
+  templateUrl: './bases.component.html',
+
+})
+export class BasesComponent  {}
